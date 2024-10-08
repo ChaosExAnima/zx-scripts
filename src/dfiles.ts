cede086216be321a1ccdc088c4504041311e60ef
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-10-08 22:47:51 +0200

    flexible docker run

--- src/dfiles.ts
+++ src/dfiles.ts
@@ -2,11 +2,27 @@ import { getServiceEnv } from 'lib/docker';
 import { showHelpAndExit } from 'lib/log';
 
 if (argv._.length !== 1) {
-	showHelpAndExit('Required args: [service]');
+	showHelpAndExit(
+		'Required args: [service] [-b]',
+		'',
+		'Flags:',
+		' b: Binds the current directory to /host in the container',
+	);
 }
 
-const [service] = argv._;
+const [service, ...rest] = argv._;
 
 const details = await getServiceEnv(service);
 
-await $`docker run --rm -it --volumes-from ${details.Name}.${details.ID} chaosexanima/toolkit zsh`;
+const flags = [
+	'--rm',
+	'-it',
+	`--volumes-from=${details.Name}.${details.ID}`,
+	...rest,
+];
+if (argv.b) {
+	flags.push(`-v=${process.cwd()}:/host`);
+}
+flags.push('chaosexanima/toolkit', 'zsh');
+
+await $`docker run ${flags}`;
